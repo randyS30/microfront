81662c47e68c4bf1378effe46f447f6fcd874001
randyS30 <randy_saravia@usmp.pe>
2024-08-14 03:04:54 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import Home from './components/Home'
+import './App.css'
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+
+
+function App() {
+  
+
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
